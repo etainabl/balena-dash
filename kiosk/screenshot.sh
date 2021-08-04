b5d6eadd4454640ccd9c113419a0baae3bc662ea
Jonathan Lambert <jlambert@etainabl.com>
2021-08-04 12:48:40 +0100

    Update screenshot

--- kiosk/screenshot.sh
+++ kiosk/screenshot.sh
@@ -1,6 +1,10 @@
+#!/bin/sh
+
 export DISPLAY=:0
 export XAUTHORITY=/home/chromium/.Xauthority
+TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
+
 xwd -root -out /home/chromium/sc.xwd
 convert /home/chromium/sc.xwd /home/chromium/sc.jpg
-aws s3 cp /home/chromium/sc.jpg s3://etainabl-kiosk/screenshots/test.jpg
+aws s3 cp --quiet  /home/chromium/sc.jpg s3://etainabl-kiosk/screenshots/${RESIN_DEVICE_NAME_AT_INIT}_${TIMESTAMP}.jpg
 rm -rf /home/chromium/sc.jpg
