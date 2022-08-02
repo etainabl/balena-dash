4a80223bcde7e104e865804c619d6e50dce01697
Jonathan Lambert <jonathanprl@users.noreply.github.com>
2022-08-02 21:48:19 +0100

    Create screenshot_multi.sh

---
+++ kiosk/screenshot_multi.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Taking screenshot..."
+/home/chromium/screenshot.sh
+sleep 30
+
+echo "Taking screenshot..."
+/home/chromium/screenshot.sh
+sleep 30
+
+echo "Taking screenshot..."
+/home/chromium/screenshot.sh
+sleep 30
+
+echo "Taking screenshot..."
+/home/chromium/screenshot.sh
+sleep 30
+
+echo "Done!"
